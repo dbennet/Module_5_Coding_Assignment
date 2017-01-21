48da9cefd75a7b19ff17ee38ce05d801200d0bf3
David Bennett <david.bennett@asml.com>
2017-01-21 17:51:13 +0100

    changing format

--- src/public/myinfo/myinfo.controller.js
+++ src/public/myinfo/myinfo.controller.js
@@ -7,7 +7,7 @@ angular.module('public')
 MyInfoController.$inject = ['UserInfoService','MenuService'];
 function MyInfoController(UserInfoService, MenuService) {
   var $info = this;  
-  $info.info = UserInfoService.getInfo;  
+  $info.info = UserInfoService.getInfo();  
   
     MenuService.getItem($info.info.favorite).then(function(response) {
          $info.menuItem = response;
