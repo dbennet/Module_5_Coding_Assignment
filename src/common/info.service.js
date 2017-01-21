54a8ab17f5926b7d0f9055bd54c50a9935003b09
David Bennett <david.bennett@asml.com>
2017-01-21 16:25:22 +0100

    fix sign up controller syntax and add info service

---
+++ src/common/info.service.js
@@ -0,0 +1,18 @@
+(function () {
+"use strict";
+
+angular.module('common')
+.service('UserInfoService', UserInfoService);
+
+UserInfoService.$inject = [];
+function UserInfoService() {
+  var service = this;
+  
+  service.setInfo = function (info) {
+    service.info = info;
+  }
+  
+  service.getInfo = function () {
+     return service.info;
+    }
+}
--- src/public/signup/signup.controller.js
+++ src/public/signup/signup.controller.js
@@ -4,14 +4,24 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['MenuService'];
+SignUpController.$inject = ['MenuService','UserInfoService'];
 function SignUpController(MenuService) {
   var $sgn = this;
   $ctrl.info = {};
   
+   $ctrl.submit = function() {
+      MenuService.getMenuItem($ctrl.info.favorite)
+        .then(function(response) {
+          $ctrl.invalidFavorite = false;
+          $ctrl.submitted = true;
+          UserInfoService.setInfo($ctrl.info);
+        })
+        .catch(function() {
+          $ctrl.invalidFavorite = true;
+        });
+    }
   
-  
-  $sgn validateFav = function() {
+  $sgn.validateFav = function() {
     var resp = MenuService.getItem($sgn.info.favorite);
     console.log(resp);
    }
